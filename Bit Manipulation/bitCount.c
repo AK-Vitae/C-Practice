6afe7b41439c22e52a9ba748acc6ce19e7073119
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-10-15 17:00:15 -0400

    Added code for reading in argument as well as the method for even or odd parity

--- Bit Manipulation/bitCount.c
+++ Bit Manipulation/bitCount.c
@@ -11,5 +11,20 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    unsigned short int n = atoi(argv[1]);
+    // printf("Number: %d\n", n);
+
     return EXIT_SUCCESS;
+}
+
+void evenOddParity(int n, int numberOfPairs)
+{
+    if (n % 2 == 0)
+    {
+        printf("Even-Parity\t%d\n", numberOfPairs);
+    }
+    else
+    {
+        printf("Odd-Parity\t%d\n", numberOfPairs);
+    }
 }
