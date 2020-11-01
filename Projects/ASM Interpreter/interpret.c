0934561b32d70c174fcc84c48af4df4082c15441
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-11-01 08:51:39 -0500

    Added file handling parameters

--- Projects/ASM Interpreter/interpret.c
+++ Projects/ASM Interpreter/interpret.c
@@ -9,4 +9,11 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error\nProper Usage: ./interpret file.asm\n");
         exit(EXIT_FAILURE);
     }
+
+    char lines[100][20];
+    int lineCount = 0;
+    FILE *file = fopen(argv[1], "r");
+
+    fseek(file, 0, SEEK_END);
+    fclose(file);
 }
