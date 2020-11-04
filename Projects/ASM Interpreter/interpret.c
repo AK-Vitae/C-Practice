6791faad27a936e48565313f92ee956cb3a9ec6e
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-11-04 14:24:09 -0500

    Added code to identify the first instruction keyword

--- Projects/ASM Interpreter/interpret.c
+++ Projects/ASM Interpreter/interpret.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXLINES 100 
-#define MAXLINELENGTH 15 
+#define MAXLINES 100
+#define MAXLINELENGTH 15
 
 int main(int argc, char *argv[])
 {
@@ -18,19 +18,32 @@ int main(int argc, char *argv[])
     int lineCount = 0;
     char lines[MAXLINES][MAXLINELENGTH];
     FILE *file = fopen(argv[1], "r");
-    while(fgets(lines[lineCount], MAXLINELENGTH, file)) 
-	{
+    while (fgets(lines[lineCount], MAXLINELENGTH, file))
+    {
         lines[lineCount][strlen(lines[lineCount]) - 1] = '\0';
         lineCount++;
     }
-
     fclose(file);
 
-    printf("Number of lines: %d\n", lineCount);
-    printf("The contents of the file are: \n");    
-    for(int i = 0; i < lineCount; ++i)
+    // Intialize potential registers
+    int ax;
+    int bx;
+    int cx;
+    int dx;
+
+    // Carry out instructions
+    for (int i = 0; i < lineCount; i++)
     {
-        printf("%s\n", lines[i]);
+        char instruction[5];
+        int n = sscanf(lines[i], "%s", instruction);
+        int instructionValue = i+1;
+        if (n == 1)
+        {
+            printf("Instruction %d: %s\n", instructionValue, instruction);
+        }
+        else
+        {
+            printf("Instruction %d: No operation\n", instructionValue);
+        }
     }
-    printf("\n");
 }
