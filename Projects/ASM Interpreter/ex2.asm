e46122698e0f5b003912068475fce06978af381d
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-11-05 19:35:55 -0500

    Added code for reading in register data and printing it out

--- Projects/ASM Interpreter/ex2.asm
+++ Projects/ASM Interpreter/ex2.asm
@@ -5,3 +5,4 @@ print 0
 sub 1 ax
 jle 7 ax 0
 jmp 3
+
--- Projects/ASM Interpreter/interpret.c
+++ Projects/ASM Interpreter/interpret.c
@@ -36,14 +36,67 @@ int main(int argc, char *argv[])
     {
         char instruction[5];
         int n = sscanf(lines[i], "%s", instruction);
-        int instructionValue = i+1;
+        int instructionLine = i + 1;
         if (n == 1)
         {
-            printf("Instruction %d: %s\n", instructionValue, instruction);
+            
+            printf("Instruction %d: %s\n", instructionLine, instruction);
+            if (strcmp(instruction, "read") == 0)
+            {
+                char reg[2];
+                sscanf(lines[i], "%s %s", instruction, reg);
+
+                if (strcmp(reg, "ax") == 0)
+                {
+                    scanf("%d", &ax);
+                    printf(" Register Name: %s\n", reg);
+                    printf(" Register Value: %d\n", ax);
+                }
+                else if (strcmp(reg, "bx") == 0)
+                {
+                    scanf("%d", &bx);
+                    printf(" Register Name: %s\n", reg);
+                    printf(" Register Value: %d\n", bx);
+                }
+                else if (strcmp(reg, "cx") == 0)
+                {
+                    scanf("%d", &cx);
+                    printf(" Register Name: %s\n", reg);
+                    printf(" Register Value: %d\n", cx);
+                }
+                else if (strcmp(reg, "dx") == 0)
+                {
+                    scanf("%d", &dx);
+                    printf(" Register Name: %s\n", reg);
+                    printf(" Register Value: %d\n", dx);
+                }
+            }
+            else if (strcmp(instruction, "print") == 0)
+            {
+                char reg[2];
+                sscanf(lines[i], "%s %s", instruction, reg);
+
+                if (strcmp(reg, "ax") == 0)
+                {
+                    printf("%d", ax);
+                }
+                else if (strcmp(reg, "bx") == 0)
+                {
+                    printf("%d", bx);
+                }
+                else if (strcmp(reg, "cx") == 0)
+                {
+                    printf("%d", cx);
+                }
+                else if (strcmp(reg, "dx") == 0)
+                {
+                    printf("%d", dx);
+                }
+            }
         }
         else
         {
-            printf("Instruction %d: No operation\n", instructionValue);
+            printf("Instruction %d: No operation\n", instructionLine);
         }
     }
 }
