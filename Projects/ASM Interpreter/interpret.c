c3a31aad6f62c15e6b8051f7587f20387d9d15ab
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-11-10 21:09:41 -0500

    Cleaned up redundant code

--- Projects/ASM Interpreter/interpret.c
+++ Projects/ASM Interpreter/interpret.c
@@ -6,9 +6,10 @@
 #define MAXLINES 100
 #define MAXLINELENGTH 20
 
-//Prototypes
+// Prototypes
 int getValue(char reg[], int ax, int bx, int cx, int dx);
 
+// Driver
 int main(int argc, char *argv[])
 {
     // Arguments not properly provided
@@ -44,213 +45,185 @@ int main(int argc, char *argv[])
         if (n == 1)
         {
             // printf("Instruction %d: %s\n", instructionLine, instruction);
-            // READ //
-            if (strcmp(instruction, "read") == 0)
+            // read and print //
+            if ((strcmp(instruction, "read") == 0) || (strcmp(instruction, "print") == 0))
             {
                 char reg[10];
                 sscanf(lines[i], "%s %s", instruction, reg);
 
-                int condition = (reg[0] - 97) + (reg[1] - 120); // a = 97, b = 98, c = 99, d = 100, x = 120
-                switch (condition)
+                if (strcmp(instruction, "read") == 0)
                 {
-                case 0:
-                    scanf("%d", &ax);
-                    break;
-                case 1:
-                    scanf("%d", &bx);
-                    break;
-                case 2:
-                    scanf("%d", &cx);
-                    break;
-                case 3:
-                    scanf("%d", &dx);
-                    break;
-                default:
-                    printf("Invalid register");
-                    break;
-                }
-            }
-            // PRINT //
-            else if (strcmp(instruction, "print") == 0)
-            {
-                char reg[10];
-                sscanf(lines[i], "%s %s", instruction, reg);
-
-                if (isdigit(reg[0]) != 0)
-                {
-                    int value;
-                    sscanf(reg, "%d", &value);
-                    printf("%d", value);
-                }
-                else
-                {
-                    int condition = (reg[0] - 97) + (reg[1] - 120);
+                    int condition = (reg[0] - 97) + (reg[1] - 120); // a = 97, b = 98, c = 99, d = 100, x = 120
                     switch (condition)
                     {
                     case 0:
-                        printf("%d", ax);
+                        scanf("%d", &ax);
                         break;
                     case 1:
-                        printf("%d", bx);
+                        scanf("%d", &bx);
                         break;
                     case 2:
-                        printf("%d", cx);
+                        scanf("%d", &cx);
                         break;
                     case 3:
-                        printf("%d", dx);
+                        scanf("%d", &dx);
                         break;
                     default:
-                        printf("Print Error");
+                        printf("Invalid register");
+                        break;
+                    }
+                }
+                else if (strcmp(instruction, "print") == 0)
+                {
+                    if (isdigit(reg[0]) != 0)
+                    {
+                        int value;
+                        sscanf(reg, "%d", &value);
+                        printf("%d", value);
+                    }
+                    else
+                    {
+                        int condition = (reg[0] - 97) + (reg[1] - 120);
+                        switch (condition)
+                        {
+                        case 0:
+                            printf("%d", ax);
+                            break;
+                        case 1:
+                            printf("%d", bx);
+                            break;
+                        case 2:
+                            printf("%d", cx);
+                            break;
+                        case 3:
+                            printf("%d", dx);
+                            break;
+                        default:
+                            printf("Print Error");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // mov, add, sub, mul, and div //
+            else if ((strcmp(instruction, "mov") == 0) || (strcmp(instruction, "add") == 0) || (strcmp(instruction, "sub") == 0) || (strcmp(instruction, "mul") == 0) || (strcmp(instruction, "div") == 0))
+            {
+                char reg1[10];
+                char reg2[10];
+                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
+
+                int x = getValue(reg1, ax, bx, cx, dx);
+
+                int condition = (reg2[0] - 97) + (reg2[1] - 120);
+
+                if ((strcmp(instruction, "mov") == 0))
+                {
+                    switch (condition)
+                    {
+                    case 0:
+                        ax = x;
+                        break;
+                    case 1:
+                        bx = x;
+                        break;
+                    case 2:
+                        cx = x;
+                        break;
+                    case 3:
+                        dx = x;
+                        break;
+                    default:
+                        printf("Movement Error");
+                        break;
+                    }
+                }
+                else if (strcmp(instruction, "add") == 0)
+                {
+                    switch (condition)
+                    {
+                    case 0:
+                        ax = ax + x;
+                        break;
+                    case 1:
+                        bx = bx + x;
+                        break;
+                    case 2:
+                        cx = cx + x;
+                        break;
+                    case 3:
+                        dx = dx + x;
+                        break;
+                    default:
+                        printf("Addition Error");
+                        break;
+                    }
+                }
+                else if (strcmp(instruction, "sub") == 0)
+                {
+                    switch (condition)
+                    {
+                    case 0:
+                        ax = ax - x;
+                        break;
+                    case 1:
+                        bx = bx - x;
+                        break;
+                    case 2:
+                        cx = cx - x;
+                        break;
+                    case 3:
+                        dx = dx - x;
+                        break;
+                    default:
+                        printf("Subtraction Error");
+                        break;
+                    }
+                }
+                else if (strcmp(instruction, "mul") == 0)
+                {
+                    switch (condition)
+                    {
+                    case 0:
+                        ax = ax * x;
+                        break;
+                    case 1:
+                        bx = bx * x;
+                        break;
+                    case 2:
+                        cx = cx * x;
+                        break;
+                    case 3:
+                        dx = dx * x;
+                        break;
+                    default:
+                        printf("Mutliplication Error");
+                        break;
+                    }
+                }
+                else if (strcmp(instruction, "div") == 0)
+                {
+                    switch (condition)
+                    {
+                    case 0:
+                        ax = x / ax;
+                        break;
+                    case 1:
+                        bx = x / bx;
+                        break;
+                    case 2:
+                        cx = x / cx;
+                        break;
+                    case 3:
+                        dx = x / dx;
+                        break;
+                    default:
+                        printf("Division Error");
                         break;
                     }
                 }
             }
-            // Move //
-            else if (strcmp(instruction, "mov") == 0)
-            {
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-
-                int condition = (reg2[0] - 97) + (reg2[1] - 120);
-                switch (condition)
-                {
-                case 0:
-                    ax = x;
-                    break;
-                case 1:
-                    bx = x;
-                    break;
-                case 2:
-                    cx = x;
-                    break;
-                case 3:
-                    dx = x;
-                    break;
-                default:
-                    printf("Movement Error");
-                    break;
-                }
-            }
-            // ARITHMETIC //
-            else if (strcmp(instruction, "add") == 0)
-            {
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-
-                int condition = (reg2[0] - 97) + (reg2[1] - 120);
-                switch (condition)
-                {
-                case 0:
-                    ax = ax + x;
-                    break;
-                case 1:
-                    bx = bx + x;
-                    break;
-                case 2:
-                    cx = cx + x;
-                    break;
-                case 3:
-                    dx = dx + x;
-                    break;
-                default:
-                    printf("Addition Error");
-                    break;
-                }
-            }
-
-            else if (strcmp(instruction, "sub") == 0)
-            {
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-
-                int condition = (reg2[0] - 97) + (reg2[1] - 120);
-                switch (condition)
-                {
-                case 0:
-                    ax = ax - x;
-                    break;
-                case 1:
-                    bx = bx - x;
-                    break;
-                case 2:
-                    cx = cx - x;
-                    break;
-                case 3:
-                    dx = dx - x;
-                    break;
-                default:
-                    printf("Subtraction Error");
-                    break;
-                }
-            }
-
-            else if (strcmp(instruction, "mul") == 0)
-            {
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-
-                int condition = (reg2[0] - 97) + (reg2[1] - 120);
-                switch (condition)
-                {
-                case 0:
-                    ax = ax * x;
-                    break;
-                case 1:
-                    bx = bx * x;
-                    break;
-                case 2:
-                    cx = cx * x;
-                    break;
-                case 3:
-                    dx = dx * x;
-                    break;
-                default:
-                    printf("Mutliplication Error");
-                    break;
-                }
-            }
-
-            else if (strcmp(instruction, "div") == 0)
-            {
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %s %s", instruction, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-
-                int condition = (reg2[0] - 97) + (reg2[1] - 120);
-                switch (condition)
-                {
-                case 0:
-                    ax = x / ax;
-                    break;
-                case 1:
-                    bx = x / bx;
-                    break;
-                case 2:
-                    cx = x / cx;
-                    break;
-                case 3:
-                    dx = x / dx;
-                    break;
-                default:
-                    printf("Division Error");
-                    break;
-                }
-            }
-            // JUMPS //
+            // jmp //
             else if (strcmp(instruction, "jmp") == 0)
             {
                 int jump;
@@ -258,7 +231,8 @@ int main(int argc, char *argv[])
                 // printf(" Jumped to line %d\n", jump);
                 i = jump - 1;
             }
-            else if (strcmp(instruction, "je") == 0)
+            // je, jne, jg, jge, jl, and jle //
+            else if ((strcmp(instruction, "je") == 0) || (strcmp(instruction, "jne") == 0) || (strcmp(instruction, "jg") == 0) || (strcmp(instruction, "jge") == 0) || (strcmp(instruction, "jl") == 0) || (strcmp(instruction, "jle") == 0))
             {
                 int jump;
                 char reg1[10];
@@ -269,92 +243,51 @@ int main(int argc, char *argv[])
                 int x = getValue(reg1, ax, bx, cx, dx);
                 int y = getValue(reg2, ax, bx, cx, dx);
 
-                if (x = y)
+                if (strcmp(instruction, "je") == 0)
                 {
-                    i = jump - 1;
+                    if (x = y)
+                    {
+                        i = jump - 1;
+                    }
                 }
-            }
-            else if (strcmp(instruction, "jne") == 0)
-            {
-                int jump;
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %d %s %s", instruction, &jump, reg1, reg2);
-                // printf(" Jumped to line %d, %s %s\n", jump, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-                int y = getValue(reg2, ax, bx, cx, dx);
-
-                if (x != y)
+                else if (strcmp(instruction, "jne") == 0)
                 {
-                    i = jump - 1;
+                    if (x != y)
+                    {
+                        i = jump - 1;
+                    }
                 }
-            }
-            else if (strcmp(instruction, "jg") == 0)
-            {
-                int jump;
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %d %s %s", instruction, &jump, reg1, reg2);
-                // printf(" Jumped to line %d, %s %s\n", jump, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-                int y = getValue(reg2, ax, bx, cx, dx);
-
-                if (x > y)
+                else if (strcmp(instruction, "jg") == 0)
                 {
-                    i = jump - 1;
+                    if (x > y)
+                    {
+                        i = jump - 1;
+                    }
                 }
-            }
-            else if (strcmp(instruction, "jge") == 0)
-            {
-                int jump;
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %d %s %s", instruction, &jump, reg1, reg2);
-                // printf(" Jumped to line %d, %s %s\n", jump, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-                int y = getValue(reg2, ax, bx, cx, dx);
-
-                if (x >= y)
+                else if (strcmp(instruction, "jge") == 0)
                 {
-                    i = jump - 1;
+                    if (x >= y)
+                    {
+                        i = jump - 1;
+                    }
                 }
-            }
-            else if (strcmp(instruction, "jl") == 0)
-            {
-                int jump;
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %d %s %s", instruction, &jump, reg1, reg2);
-                // printf(" Jumped to line %d, %s %s\n", jump, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-                int y = getValue(reg2, ax, bx, cx, dx);
-
-                if (x < y)
+                else if (strcmp(instruction, "jl") == 0)
                 {
-                    i = jump - 1;
+                    if (x < y)
+                    {
+                        i = jump - 1;
+                    }
                 }
-            }
-            else if (strcmp(instruction, "jle") == 0)
-            {
-                int jump;
-                char reg1[10];
-                char reg2[10];
-                sscanf(lines[i], "%s %d %s %s", instruction, &jump, reg1, reg2);
-                // printf(" Jumped to line %d, %s %s\n", jump, reg1, reg2);
-
-                int x = getValue(reg1, ax, bx, cx, dx);
-                int y = getValue(reg2, ax, bx, cx, dx);
-
-                if (x <= y)
+                else if (strcmp(instruction, "jle") == 0)
                 {
-                    i = jump - 1;
+                    if (x <= y)
+                    {
+                        i = jump - 1;
+                    }
                 }
             }
         }
+        // no operation //
         else
         {
             // printf("Instruction %d: No operation\n", instructionLine);
