20dc1f8b9c445d6f43b9df2d9c663409ae171e32
Akshith Ramadugu <ak.ramadugu@gmail.com>
2020-11-27 18:39:55 -0500

    Updated function prototypes and contents

--- Projects/Circuit Simulator/hw7/first.c
+++ Projects/Circuit Simulator/hw7/first.c
@@ -3,28 +3,79 @@
 #include <string.h>
 #include <math.h>
 
-
 //gcc -Wall -fsanitize=address -std=c11 first.c -o first -lm
 //./first test1.txt
 
-struct directive
+typedef struct Directive
 {
-    char gate[17];
+    char block[16];
     int n;
     int s;
     int *inputs;
     int *outputs;
     int *selectors;
-};
+} Directive;
 
 // Prototypes
 int incrementInputs(int *arr, int icount);
-void NOT(int *values, int iindex, int oindex);
-void AND(int *values, int iindex1, int iindex2, int oindex);
-void OR(int *values, int iindex1, int iindex2, int oindex);
-void NAND(int *values, int iindex1, int iindex2, int oindex);
-void NOR(int *values, int iindex1, int iindex2, int oindex);
-void XOR(int *values, int iindex1, int iindex2, int oindex);
+void not(int *arr, int inputIndex, int outputIndex);
+void and (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex);
+void or (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex);
+void nand(int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex);
+void nor(int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex);
+void xor (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex);
 int indexOf(int size, char **arr, char *var);
-void resetValues(int size, int *arr);
 
+int incrementInputs(int *arr, int icount)
+{
+    for (int i = icount + 1; i >= 2; i--)
+    {
+        arr[i] = !arr[i];
+        if (arr[i] == 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void not(int *arr, int inputIndex, int outputIndex)
+{
+    arr[inputIndex] = !arr[outputIndex];
+}
+
+void and (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex)
+{
+    arr[outputIndex] = arr[inputIndexOne] && arr[inputIndexTwo];
+}
+
+void or (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex)
+{
+    arr[outputIndex] = arr[inputIndexOne] || arr[inputIndexTwo];
+}
+
+void nand(int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex)
+{
+    arr[outputIndex] = !(arr[inputIndexOne] && arr[inputIndexTwo]);
+}
+
+void nor(int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex)
+{
+    arr[outputIndex] = !(arr[inputIndexOne] || arr[inputIndexTwo]);
+}
+
+void xor (int *arr, int inputIndexOne, int inputIndexTwo, int outputIndex) {
+    arr[outputIndex] = arr[inputIndexOne] ^ arr[inputIndexTwo];
+}
+
+    int indexOf(int size, char **arr, char *var)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (strcmp(arr[i], var) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
